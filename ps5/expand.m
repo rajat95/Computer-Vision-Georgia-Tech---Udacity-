f9b350714ef2b9522b38109f93a86f94a8e6e63b
Rajat Arora <arora.rajat428@gmail.com>
2016-12-26 23:58:15 +0530

    Add files via upload

--- ps5/expand.m
+++ ps5/expand.m
@@ -1,7 +1,13 @@
 function [out] = expand(img)
     [rows,cols] = size(img);
-    out = zeros([2*rows 2*cols 3]);
-    out(1:2:2*rows,1:2:2*cols,:) = img;
+    if(ndims(img) == 2)
+        out = zeros([2*rows 2*cols]);
+        out(1:2:2*rows,1:2:2*cols) = img;
+    else
+        out = zeros([2*rows 2*cols 3]);
+        out(1:2:2*rows,1:2:2*cols,:) = img;
+    end
+
     G = fspecial('gaussian',[5 5],1.0);
     out = imfilter(out,G);
     out = 4*out;
